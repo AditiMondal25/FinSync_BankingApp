ab2e381fe07557273092e04f432a740a974d931d
Aditi Mondal <80616089+AditiMondal25@users.noreply.github.com>
2024-06-20 16:01:42 +0530

    Delete components/ui directory

--- components/ui/SideBar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const Sidebar=({user}:SiderbarProps)=>{
-    return(
-        <section className="sidebar">
-            <nav className="flex flex-col gap-4
-        </section>
-    )
-}
-export default Sidebar
